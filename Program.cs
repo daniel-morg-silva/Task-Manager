0325ee55ef8a4e3d22c07f8083e46a51feb808f4
Daniel Silva <dms121970@gmail.com>
2025-09-19 19:51:20 +0200

    Initial commit: Basic console app with SQLite database setup

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Data.Sqlite;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TaskManager
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string ConnectionString = "Data Source = DataBase.db";
+
+            string createTable = @"CREATE TABLE IF NOT EXISTS Task(Id INTEGER PRIMARY KEY, 
+                                                                       Description TEXT,
+                                                                       IsCompleted INTEGER)";
+
+            using (SqliteConnection SQLiteConnection = new SqliteConnection(ConnectionString))
+            {
+                SQLiteConnection.Open();
+
+                using (SqliteCommand command = new SqliteCommand(createTable, SQLiteConnection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+
+                Console.WriteLine(SQLiteConnection.State);
+
+                Program.InsertData();
+
+            }
+
+
+
+
+        }
+        public static void InsertData(SqliteConnection connection)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
